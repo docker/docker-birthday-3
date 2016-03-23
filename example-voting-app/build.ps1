aee8888fa756e7134dad08b4a0f052e502edc3ed
Stefan Scherer <scherer_stefan@icloud.com>
2016-03-23 16:02:16 -0700

    Add tag
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- example-voting-app/build.ps1
+++ example-voting-app/build.ps1
@@ -1,4 +1,5 @@
 docker build -t voting-app voting-app
+docker build -t postgresql postgresql
 docker build -t worker worker
 docker build --no-cache -t result-app result-app
 docker build -t postgresql postgresql
