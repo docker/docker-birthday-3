1db5ed1d964af28b8f761e27314e91c0d2296126
Stefan Scherer <scherer_stefan@icloud.com>
2016-03-23 15:19:46 -0700

    Update config.json
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- example-voting-app/build.ps1
+++ example-voting-app/build.ps1
@@ -1,5 +1,5 @@
 docker build -t voting-app voting-app
 docker build -t worker worker
-docker build -t result-app result-app
+docker build --no-cache -t result-app result-app
 docker build -t postgresql postgresql
 docker build -t redis redis
